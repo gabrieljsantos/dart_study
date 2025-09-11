6a7d4925e5ab7b1f64166663651d9f58c4731a6f
Gabriel J Santos <93229942+gabrieljsantos@users.noreply.github.com>
2025-09-10 21:49:04 -0300

    Update 05-particulas_em_movimento.dart

--- 05-particulas_em_movimento.dart
+++ 05-particulas_em_movimento.dart
@@ -1,5 +1,38 @@
 import 'dart:math'
 
-class Particula{
+class Vetor2D{
+  double x;
+  double y;
+
+  Vetor2D(this.x,this.y);
+
+  void incrementar(double x1, double y1){
+    x += x1;
+    y += y1;
+  }
+
+  double magnitude(){
+    return (sqrt(pow(x,2)+pow(y,2)));
+  }
   
+  void limitar(double max){
+    double magLimite = magnitude();
+    if (magLimite > max){
+        double fator = max / magLimite;
+        x *= fator;
+        y *= fator;
+    }
+  }
+}
+
+class Particula {
+  Vetor2D posicao;
+  Vetor2D velocidade;
+  Vetor2D aceleracao;
+  double massa;
+
+  Particula(this.posicao, this.velocidade, this.aceleracao, this.massa);
+
+  metodos...
+
 }
